34620313f91a3025be271a7c5f360eba718f553e
José Rafael Álvarez Espino <joserrafael.ja@gmail.com>
2019-03-23 21:02:17 +0100

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get(browser.baseUrl) as Promise<any>;
+  }
+
+  getTitleText() {
+    return element(by.css('eit-root h1')).getText() as Promise<string>;
+  }
+}
